6e330f5a64deb8c48d8dc044a0f378c125be8851
Prajna <prajnapprabhu3@gmail.com>
2024-08-18 02:49:36 +0530

    feat: next-aut setup and user schema

---
+++ auth/config.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  secret: process.env.AUTH_SECRET,
+  session: { strategy: "jwt" },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENt_SECRET!,
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+});
