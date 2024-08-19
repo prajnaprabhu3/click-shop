b536c87e4f5ea9dab374bb480ca43bbf1c81093b
Prajna <prajnapprabhu3@gmail.com>
2024-08-19 20:49:47 +0530

    fix: deployment fix uploadthing/route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,16 @@
 import { createRouteHandler } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 
+// export const { GET, POST } = createRouteHandler({
+//   router: ourFileRouter,
+// });
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
+
+export default async function handler(req: Request, res: Response) {
+  if (req.method === "GET") {
+  } else if (req.method === "POST") {
+  }
+}
